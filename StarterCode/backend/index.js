128a26d4ff0d5fdd0e2010aeaca377ff0834d6e3
vicky <vicky64777@gmail.com>
2024-09-12 13:45:03 -0700

    finish test

--- StarterCode/backend/index.js
+++ StarterCode/backend/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 const cors = require('cors');
 
 app.use(express.json());
